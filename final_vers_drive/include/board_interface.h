7641dc6d03c12b0469a10b6f90ed6be417cf4bdd
EmirDns <76104355+EmirDns@users.noreply.github.com>
2022-02-12 15:08:31 +0300

    Update board_interface.h

--- final_vers_drive/include/board_interface.h
+++ final_vers_drive/include/board_interface.h
@@ -1,40 +1,65 @@
 #ifndef BOARD_INTERFACE_H
 #define BOARD_INTERFACE_H
 
-#include "Arduino.h"
-#include "ros.h"
-#include "std_msgs/Float64MultiArray.h"
-#include "std_msgs/String.h"
-#include "HardwareSerial.h"
+#include <Arduino.h>
+#include <ros.h>
+#include <std_msgs/Float64MultiArray.h>
+#include <std_msgs/String.h>
 
 class BoardInterface
 {
-    public:
+    protected:
         int _array_length;
         int _str_msg_length;
         int _str_piece_len;
         float _radian_map_coef;
-        float *command_to_send;
-        float *feedback_to_send;
+        // Parametric data that can be assigned while constructing a new class
 
+        float *commands_to_send;
+        float *feedback_to_send;
+        // Command and feedback array that is used to interface with arm or drive sys...
 
         std_msgs::Float64MultiArray feedback_arr;
         std_msgs::Float64MultiArray command_arr;
+        // Float arrays to communicate with ROS
 
         String str_command;
         String str_feedback;
+        // String arrays to communicate with MCUs 
 
     public:
-        BoardInterface(int arr_len, int str_msg_len);
+
+        BoardInterface(int arr_len, int str_msg_len, int str_piece_len, float mapping_coef);
+        // Constructor. Assings necessary data and allocates memory
+
         ~BoardInterface();
+        // Destructor. Frees the memory
 
         void assignFeedbackArr(std_msgs::Float64MultiArray &goal_arr);
+        // Assigns the feedback_arr to a global variable
+
         void assignCommandArr(const std_msgs::Float64MultiArray &given_arr);
-        std_msgs::Float64MultiArray returnFeedbackMultiArray();
+        // Assigns the data of the give command array to the private variable
+
+        std_msgs::Float64MultiArray returnFeedbackMultiArr();
+        // Returns feedback_arr
+
         void multiArrToArr();
+        // Assigns the data in the command to the commands to send array
+
         void arrToMultiArr();
+        // Assigns the data in the feedback array to the feedback multi array (std_msg)
+
         void assignStrFeedback(const String &enc_str);
-        String returnCommandString();
+        // Assigns the data to the private variable str_feedback
+
+        String returnCommandStr();
+        // Returns command as string variable
+
+        //TEST
+        float detectDirection(char direction_byte);
+        // Detects the direction (1: Positive, 0: Negative) -> Helper method for children classes' parseEncoderFeedback
+
 };
 
 #endif
